0283cd826f2e03fe80b127095fcba2f8b76dfd39
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-11 12:32:09 +0100

    card books

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -5,6 +5,12 @@
         <h1>Book Library</h1>
         <div class="row">
 
+            <!--create new book-->
+            <div class="col-12">
+                <a href="#" class="btn btn-primary">create new book</a>
+            </div>
+
+            <!--list books-->
             @foreach ($books as $book)
                 <div class="col-4">
                     <div class="card">
@@ -13,7 +19,7 @@
                             <h6 class="card-subtitle mb-2 text-body-secondary">{{ $book->author }}</h6>
                             <p class="card-text">{{ $book->description }}</p>
                             <a href="#" class="card-link text-danger">{{ $book->price }}</a>
-                            <a href="#" class="card-link">Another link</a>
+                            <a href="#" class="card-link btn btn-success">edit</a>
                         </div>
                     </div>
                 </div>
